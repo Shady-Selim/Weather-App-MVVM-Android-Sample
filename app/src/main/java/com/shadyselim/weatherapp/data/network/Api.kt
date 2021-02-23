141a2b2ea17ded7bb56ac850e81a597807fe2fc7
Shady Selim <shady.selim@hotmail.com>
2021-02-23 03:50:42 +0200

    Hiding the WeaterApi key

--- app/src/main/java/com/shadyselim/weatherapp/data/network/Api.kt
+++ app/src/main/java/com/shadyselim/weatherapp/data/network/Api.kt
@@ -1,10 +1,11 @@
 package com.shadyselim.weatherapp.data.network
 
+import com.shadyselim.weatherapp.BuildConfig
 import com.shadyselim.weatherapp.data.model.Weather
 import retrofit2.http.GET
 import retrofit2.http.Path
 
 interface Api {
-    @GET("forecast/2bb07c3bece89caf533ac9a5d23d8417/{location}")
+    @GET("forecast/${BuildConfig.Weather_Api_Key}/{location}")
     suspend fun getWeatherAsync(@Path("location") gLocation: String): Weather //59.337239,18.062381
 }
